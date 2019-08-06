dd9ec74bb4c51b045f741a706fd8961d4e3ce0de
Hien Nguyen <hiennguyen83@gmail.com>
2019-08-06 15:54:36 -0700

    add custom user model

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,10 +14,13 @@ class CommandTests(TestCase):
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
+    # path decorator
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            # side_effect to the testing function
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
+            
